5c0934188ff42a2148e84603dc1c9459b9c00ec8
majesticblue02 <majesticblue02@gmail.com>
2012-03-02 19:22:46 -0500

    Update FinalProject/src/com/finalProject/wpAlgorithm/FinalProjectActivity.java

--- FinalProject/src/com/finalProject/wpAlgorithm/FinalProjectActivity.java
+++ FinalProject/src/com/finalProject/wpAlgorithm/FinalProjectActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-#hello kevin
 public class FinalProjectActivity extends Activity {
     
 	private  final int INFINITY = Integer.MAX_VALUE;
